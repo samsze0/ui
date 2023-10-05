aee5e959d66cb4841249766e042082c19d7c2573
artizon-io <mingsum.sam@gmail.com>
2023-10-05 12:50:36 +0800

    fix next routes dynamic prop

--- src/next/routes/auth-callback.ts
+++ src/next/routes/auth-callback.ts
@@ -6,7 +6,7 @@ import { NextResponse } from "next/server";
 
 import type { NextRequest } from "next/server";
 
-export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic" as const;
 
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url);
